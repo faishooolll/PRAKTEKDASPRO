6504ae28212b516002cf104b593c1daeb9a8719b
Faishol <faisholamali746@gmail.com>
2025-10-29 11:54:02 +0700

    nilaikelompok02

---
+++ NilaiKelompok02.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class NilaiKelompok02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = 1, j, nilai;
+        float totalNilai, rataNilai;
+        while (i <= 6) {
+            System.out.println("Kelompok " + i);
+            totalNilai = 0; // reset setiap kelompok
+            for (j = 1; j <= 5; j++) {
+                System.out.print("Nilai dari Kelompok Penilai " + j + " : ");
+                nilai = sc.nextInt();
+                totalNilai += nilai; 
+            }
+            rataNilai = totalNilai / 5; 
+            System.out.println("Kelompok " + i + " rata nilainya : " + rataNilai + "\n");
+            i++;
+        }
+        sc.close();
+    }
+}
